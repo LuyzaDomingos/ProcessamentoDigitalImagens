9effeeb5e2c046ff8383012df34cc53dbca263a0
Luyza Domingos <luyzadomingos@cc.ci.ufpb.br>
2019-02-23 18:31:56 -0300

    Negativo - implementado

--- Primeiro-trabalho/main.m
+++ Primeiro-trabalho/main.m
@@ -1,9 +1,9 @@
 
 imagem = imread('flor.jpg');
-imagem_2 = imread('flor.jpg');
+%imagem_2 = imread('Shapes.png');
 
 imagem = uint8(imagem);
-imagem_2 = uint8(imagem_2);
+%imagem_2 = uint8(imagem_2);
 
 valor = input('Escolha uma opcao entre 1 e 8 : \n\n');
 
@@ -12,6 +12,8 @@ switch valor
   RgbYiq(imagem);
   case 2
   Bandas(imagem);
+  case 3
+    Negativo(imagem);
 end
 
 
