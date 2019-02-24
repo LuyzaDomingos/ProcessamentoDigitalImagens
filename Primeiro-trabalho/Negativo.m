6ffd5a8f7458487b89f4d868e4bd0d5c4a0c2413
Luyza Domingos <luyzadomingos@cc.ci.ufpb.br>
2019-02-24 18:25:02 -0300

    Atualização - Negativo

--- Primeiro-trabalho/Negativo.m
+++ Primeiro-trabalho/Negativo.m
@@ -1,13 +1,33 @@
-function negativo = Negativo(imagem)
-  
-  negativo =255 - imagem;
-  %negativo = imcomplement(imagem); função imcomplement é do octave. E computa o negativo da imagem
-  
-  figure
-  imshow(imagem);
-  title('Imagem Original');
-  
-  figure
-  imshow(negativo);
-  title("Imagem Negativa");
+function [vermelho,verde,azul,negativo] = Negativo(imagem) % função que retorna mais de um elemento -> [...]
+      
+    vermelho = Neg(Banda_r(imagem)); 
+    verde = Neg(Banda_g(imagem));
+    azul = Neg(Banda_b(imagem));
+    negativo = Neg(imagem);
+    
+    figure
+    imshow(vermelho);
+    title('negativo - red');
+     
+    figure
+    imshow(azul);
+    title('negativo - blue');
+      
+    figure
+    imshow(verde);
+    title('negativo - green');
+      
+    figure
+    imshow(imagem);
+    title('Imagem Original');
+    
+    figure
+    imshow(negativo);
+    title("Imagem Negativa");
+      
 end  
+
+%%%%%%%%%%%%Comentários%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Programa que exibe o negativo das bandas e da imagem original              %
+% E para isso usa as funções específicas Neg(imagem) e Neg(Bandas_x(imagem)).%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
